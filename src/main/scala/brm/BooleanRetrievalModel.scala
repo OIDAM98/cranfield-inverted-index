df39afe4fef3f9df1fbed25145365873e836302b
Oscar De Alva <oscar.ivan98@gmail.com>
2019-09-06 10:42:09 -0500

    Added BooleanRetrievalModel class documentation

--- src/main/scala/brm/BooleanRetrievalModel.scala
+++ src/main/scala/brm/BooleanRetrievalModel.scala
@@ -10,6 +10,11 @@ class BooleanRetrievalModel(index: InvertedIndex) {
 
   private val parser: QueryParser = new QueryParser(this)
 
+  /*
+  Converts a word into its binary representation.
+  This means that it searches through the Index if the word
+  exists and gets the Ids where the word is
+   */
   def convertToBinaryArray(word: String) = {
     index.get(word) match {
       case Some(value) =>
@@ -18,6 +23,9 @@ class BooleanRetrievalModel(index: InvertedIndex) {
             documents
               .map(_.id)
               .foldLeft(BinaryArray.fill(index.numberOfDocs)(0)) {
+                // Fills with 1 the index of the document where the word appears in
+                // Since the array is 0-based and the Ids are 1-based index then
+                // substracts 1 from the id to match the array's index
                 case (acc, index) => acc.update(index - 1, 1)
               }
         }
@@ -26,8 +34,16 @@ class BooleanRetrievalModel(index: InvertedIndex) {
     }
   }
 
+  /*
+  Parses the query and gets its results.
+  If the parser couldn't parse the query then it returns a List
+  with its head as -1
+   */
   def resolveQuery(query: String): List[Int] = {
     parser.parseAll(parser.expr, query.toLowerCase) match {
+      // Collects the results of the BinaryArray into a List
+      // of integers that represent the desired documents
+      // that match the query
       case parser.Success(result, _) =>
         result.toArray.zipWithIndex.foldRight(List.empty[Int]) {
           case ((value, i), acc) => if (value == 1) i + 1 :: acc else acc
